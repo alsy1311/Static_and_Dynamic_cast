69015b108dcce9077c1b35743f7248f805c75106
Alsy <alsy1311@yandex.ru>
2020-02-24 20:25:25 +0300

    как работают эти функции static and dinamic cast

--- Homework21/Source.cpp
+++ Homework21/Source.cpp
@@ -5,8 +5,7 @@ template<typename Derived, typename Base, typename Del>
 unique_ptr<Derived, Del>
 static_unique_ptr_cast(unique_ptr<Base, Del>&& p)
 {
-
-	auto d = static_cast<Derived *>(p.release());
+	auto d = static_cast<Derived *>(p.release()); 
 	return unique_ptr<Derived, Del>(d,
 		move(p.get_deleter()));
 }
@@ -16,11 +15,56 @@ dynamic_unique_ptr_cast(unique_ptr<Base, Del>&& p)
 {
 	if (Derived *result = dynamic_cast<Derived *>(p.get())) {
 		p.release();
+
 		return unique_ptr<Derived, Del>(result,
 			move(p.get_deleter()));
 	}
 	return unique_ptr<Derived, Del>(nullptr, p.get_deleter());
 }
+/*Принцип работы static and dynamic cast : 
+
+Base -> Base* -> Derived* -> Derived 
+
+создаем обычные указатели копируя в них умные, переприсваиваем Base* = Derived * и с помощью конструктора создаем unique pointer типа Derived который указывает на Base 
+моя костыльная попытка это сделать 
+auto basePtr = std::make_unique<Base>();
+
+	std::cout << "Base pointer say: ";
+
+	basePtr->f();
+
+	auto derivedPtr = std::make_unique<Derived>();
+
+	std::cout << "Derived pointer say: ";
+
+	derivedPtr->f();
+
+	Base* ptr = basePtr.get();
+
+	std::cout << "Base pointer say: ";
+
+	ptr->f();
+
+	Derived* ptr2 = derivedPtr.get();
+
+	std::cout << "Derived pointer say: ";
+
+	ptr2->f();
+
+	ptr = ptr2;
+
+	std::cout << "Base pointer say: ";
+
+	ptr->f();
+
+	auto ptr4 = derivedPtr.release();
+
+	auto basePtr2 = unique_ptr<Base>(ptr);
+
+
+
+	basePtr2->f();
+*/
 
 struct Base
 {
